41fa447a18e4a0ef00a37247b7804f6a2d171753
Your Name <brayan@example.com>
2023-07-13 18:54:04 -0500

    entregable

--- app/src/main/java/com/example/bibliotecadigital/ResourceDetailActivity.java
+++ app/src/main/java/com/example/bibliotecadigital/ResourceDetailActivity.java
@@ -5,7 +5,6 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -16,6 +15,7 @@ public class ResourceDetailActivity extends AppCompatActivity implements View.On
     private EditText Autortv;
     private EditText Descripciontv;
     private Button botonBuscar;
+    private Button redirectToSearchButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,8 +26,10 @@ public class ResourceDetailActivity extends AppCompatActivity implements View.On
         Autortv = findViewById(R.id.Autortv);
         Descripciontv = findViewById(R.id.Descripciontv);
         botonBuscar = findViewById(R.id.botonBuscar);
+        redirectToSearchButton = findViewById(R.id.redirectToSearchButton);
 
         botonBuscar.setOnClickListener(this);
+        redirectToSearchButton.setOnClickListener(this);
 
         String title = getIntent().getStringExtra("title");
         String author = getIntent().getStringExtra("author");
@@ -37,17 +39,26 @@ public class ResourceDetailActivity extends AppCompatActivity implements View.On
         Autortv.setText(author);
         Descripciontv.setText(description);
     }
+
     @Override
     public void onClick(View v) {
         if (v.getId() == R.id.botonBuscar) {
             buscarLibro();
+        } else if (v.getId() == R.id.redirectToSearchButton) {
+            redirectToSearchActivity();
         }
     }
+
     private void buscarLibro() {
         String titulo = Titulotv.getText().toString();
         String autor = Autortv.getText().toString();
         String descripcion = Descripciontv.getText().toString();
         String mensaje = "Búsqueda de libro\nTítulo: " + titulo + "\nAutor: " + autor + "\nDescripción: " + descripcion;
         Toast.makeText(this, mensaje, Toast.LENGTH_SHORT).show();
+    }
+
+    private void redirectToSearchActivity() {
+        Intent intent = new Intent(this, SearchActivity.class);
+        startActivity(intent);
     }
 }
